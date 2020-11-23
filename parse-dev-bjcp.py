5ce7b1920a9cc88a413417287b5274dd1c8e359d
Barry Forrest <forrbar@hilti.com>
2020-11-23 16:54:24 -0600

    tweaked to output a separate category list

--- parse-dev-bjcp.py
+++ parse-dev-bjcp.py
@@ -6,19 +6,49 @@ import re
 from bs4 import BeautifulSoup
 
 os.chdir('./dev.bjcp.org')
+categories = []
 
 
-def get_content(domset, className):
+def get_content(domset, classname):
     content = ''
     try:
         content = domset.find(
-            'div', {'class': className}).text
+            'div', {'class': classname}).text
     except AttributeError:
         print('attribute error')
     except RuntimeError:
-        print('classname %s not found' % className)
+        print('classname %s not found' % classname)
 
     return content
+
+
+def parse_style(domset):
+    appearance = get_content(domset, 'appearance')
+    aroma = get_content(domset, 'aroma')
+    flavor = get_content(domset, 'flavor')
+    mouthfeel = get_content(domset, 'mouthfeel')
+    comments = get_content(domset, 'comments')
+    comparison = get_content(domset, 'style-comparison')
+    history = get_content(domset, 'history')
+    ingredients = get_content(domset, 'ingredients')
+    statistics = get_content(domset, 'vital-statistics')
+    commercial = get_content(domset, 'commercial-examples')
+    attributes = get_content(domset, 'style-attributes')
+    impression = get_content(domset, 'overall-impression')
+    mystyle = {'impression': impression,
+               'appearance': appearance,
+               'aroma': aroma,
+               'flavor': flavor,
+               'mouthfeel': mouthfeel,
+               'comments': comments,
+               'comparison': comparison,
+               'history': history,
+               'ingredients': ingredients,
+               'statistics': statistics,
+               'commercial': commercial,
+               'attributes': attributes
+               }
+    return mystyle
 
 
 def parse_file(filename):
@@ -27,51 +57,29 @@ def parse_file(filename):
         broth = BeautifulSoup(content, 'lxml')
         articles = broth.findAll('article')
 
-        categoryTag = articles[0]
-        header = categoryTag.find('h1')
+        categorytag = articles[0]
+        header = categorytag.find('h1')
         category = header.text.strip('\t\n')
-        description = get_content(categoryTag, 'entry-content')
+        description = get_content(categorytag, 'entry-content')
         # first one is a category description
-        print(category)
-        print(description)
 
-        top = len(articles)
-        for substyle in range(1, top):
-            style = articles[substyle].find('h1').text.strip('\t\n')
-            impression = get_content(articles[substyle], 'overall-impression')
-            appearance = get_content(articles[substyle], 'appearance')
-            aroma = get_content(articles[substyle], 'aroma')
-            flavor = get_content(articles[substyle], 'flavor')
-            mouthfeel = get_content(articles[substyle], 'mouthfeel')
-            comments = get_content(articles[substyle], 'comments')
-            comparison = get_content(articles[substyle], 'style-comparison')
-            history = get_content([substyle], 'history')
-            ingredients = get_content(articles[substyle], 'ingredients')
-            statistics = get_content(articles[substyle], 'vital-statistics')
-            commercial = get_content(articles[substyle], 'commercial-examples')
-            attributes = get_content(articles[substyle], 'style-attributes')
+        categories.append({'category': category, 'description': description, })
 
-            print('style %s' % style)
-            myStyle = {'name': style,
-                       'category': category,
-                       'entry-content': description,
-                       'impression': impression,
-                       'appearance': appearance,
-                       'aroma': aroma,
-                       'flavor': flavor,
-                       'mouthfeel': mouthfeel,
-                       'comments': comments,
-                       'comparison': comparison,
-                       'history': history,
-                       'ingredients': ingredients,
-                       'statistics': statistics,
-                       'commercial': commercial,
-                       'attributes': attributes
-                       }
-            path = '/Users/Barry/Projects/BeerStyles/BJCP_2015/json-data/%s.json' % style
-            fj = open(path, 'w')
-            j = json.dump(myStyle, fj,  sort_keys=False, indent=4)
-            print(j)
+        # top = len(articles)
+        # for substyle in range(1, top):
+        #     style = articles[substyle].find('h1').text.strip('\t\n')
+
+        #     mystyle = parse_style(articles[substyle])
+
+        #     print('style %s' % style)
+        #     mystyle['name'] = style
+        #     mystyle['category'] = category
+        #     mystyle['entry-content'] = description
+
+        #     path = '/Users/Barry/Projects/BeerStyles/BJCP_2015/json-data/%s.json' % style
+        #     fj = open(path, 'w')
+        #     j = json.dump(mystyle, fj,  sort_keys=False, indent=4)
+        #     print(j)
 
 
 data = [f for f in glob.glob("*.html")]
@@ -79,4 +87,9 @@ data = [f for f in glob.glob("*.html")]
 for datum in data:
     print(datum)
     parse_file(datum)
+
+catpath = '/Users/barryforrest/Projects/MyGithub/BJCP_2015/json-data/categories.json'
+with open(catpath, 'w') as fc:
+    j = json.dump(categories, fc, indent=4)
+    print(j)
 # pass dom-set and class name, get the inner-text
